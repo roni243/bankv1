0eeaaf3ac37742bba156e4b931ba9ec40c53d9d7
roni <ksri2457@gmail.com>
2025-03-26 16:43:24 +0900

    26-5

--- src/main/java/com/metacoding/bankv1/account/history/HistoryRepository.java
+++ src/main/java/com/metacoding/bankv1/account/history/HistoryRepository.java
@@ -11,7 +11,7 @@ public class HistoryRepository {
     private final EntityManager em;
 
     public void save(int withdrawNumber, int depositNumber, int amount, int withdrawBalance, int depositBalance) {
-        Query query = em.createNativeQuery("insert into history_tb(withdraw_number, deposit_number, amount, withdraw_balance, deposit_balance, created_at) values (?, ?, ?, ?, now())");
+        Query query = em.createNativeQuery("insert into history_tb(withdraw_number, deposit_number, amount, withdraw_balance, deposit_balance, created_at) values (?, ?, ?, ?, ?, now())");
         //변경하려면 서비스에서변경, 멱등, 들어간게 그대로, 변수명 정해져 있다.
         query.setParameter(1, withdrawNumber);
         query.setParameter(2, depositNumber);
